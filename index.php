b4ca51095c252c618ba0f2bd51eee413e61287bf
n9u93n <n9u93n@gmail.com>
2025-05-11 05:49:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -112,7 +112,7 @@
             <h2 class="fade-in" data-delay="0">Join Our Community</h2>
             <p class="fade-in" data-delay="200">Be a part of the Vina Network revolution. Connect with us and stay updated!</p>
             <div class="community-links fade-in" data-delay="400">
-                <a href="https://twitter.com/vinanetwork" target="_blank" class="community-link">
+                <a href="https://x.com/Vina_Network" target="_blank" class="community-link">
                     <i class="fab fa-twitter"></i> Twitter
                 </a>
                 <a href="https://discord.gg/wm2H5epF" target="_blank" class="community-link">
