ab43b31a525dbf31c0250c16ec4e205f15b82196
Vina Network <n9u93n@gmail.com>
2025-05-23 15:57:59 +0700

    Update and rename notification.php to index.php

---
+++ notification/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Notification - Vina Network</title>
+    <link rel="stylesheet" href="../css/vina.css">
+    <link rel="stylesheet" href="notification.css">
+    <link rel="stylesheet" href="../css/all.css">
+    <link rel="stylesheet" href="../css/poppins.css">
+    <!-- Preload critical resources -->
+    <link rel="preload" href="../webfonts/fa-brands-400.woff2" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="../webfonts/fa-regular-400.woff2" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="../webfonts/fa-solid-900.woff2" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="../fonts/poppins-400.woff2" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="../fonts/poppins-600.woff2" as="font" type="font/woff2" crossorigin>
+</head>
+<body>
+    <?php include '../include/header.php'; ?>
+
+    <section class="notification-section">
+        <div class="notification-content fade-in">
+            <i class="fas fa-tools notification-icon"></i>
+            <h1>Products Under Development</h1>
+            <p>We’re sorry, but our products are currently under development. Our team is working hard to bring you the best experience. Stay tuned for updates!</p>
+            <a href="https://www.vina.network/" class="cta-button">Back to Home</a>
+        </div>
+    </section>
+
+    <?php include '../include/footer.php'; ?>
+    <script src="../js/vina.js"></script>
+</body>
+</html>
