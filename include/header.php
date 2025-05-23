da7959b3164bf0fa90e36a2ae3f8e08a1ee32718
Vina Network <n9u93n@gmail.com>
2025-05-23 15:54:19 +0700

    Update header.php

--- include/header.php
+++ include/header.php
@@ -10,10 +10,10 @@
         <li class="dropdown">
             <a href="#" class="nav-link dropdown-toggle"><i class="fas fa-box"></i> Product</a>
             <ul class="dropdown-menu">
-                <li><a href="https://www.vina.network/notification/" class="dropdown-item"><i class="fas fa-wallet"></i> Vina Wallet</a></li>
+                <li><a href="https://www.vina.network/notification" class="dropdown-item"><i class="fas fa-wallet"></i> Vina Wallet</a></li>
                 <li><a href="https://www.vina.network/notification" class="dropdown-item"><i class="fas fa-exchange-alt"></i> Vina Dex</a></li>
-                <li><a href="https://www.vina.network/notification/" class="dropdown-item"><i class="fas fa-search"></i> Vina Scan</a></li>
-                <li><a href="https://www.vina.network/notification/" class="dropdown-item"><i class="fas fa-chart-line"></i> Vina Investment</a></li>
+                <li><a href="https://www.vina.network/notification" class="dropdown-item"><i class="fas fa-search"></i> Vina Scan</a></li>
+                <li><a href="https://www.vina.network/notification" class="dropdown-item"><i class="fas fa-chart-line"></i> Vina Investment</a></li>
             </ul>
         </li>
         <li class="dropdown">
