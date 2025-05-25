5d53a9aab2e555a32c315a20f8d5dc9b758adfe7
Vina Network <n9u93n@gmail.com>
2025-05-25 11:55:41 +0700

    Create config.php

---
+++ tools/config.php
@@ -0,0 +1,5 @@
+<?php
+// config.php
+define('HELIUS_API_KEY', '8eb75cd9-015a-4e24-9de2-5be9ee0f1c63');
+define('ERROR_LOG_PATH', '/home/hthxhyqf/domains/vina.network/public_html/tools/error_log.txt');
+?>
