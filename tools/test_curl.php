9d10c890c2edc9218db30e817704b7861a22f80d
Vina Network <n9u93n@gmail.com>
2025-05-26 04:49:12 +0700

    Update test_curl.php

--- tools/test_curl.php
+++ tools/test_curl.php
@@ -1,11 +1,5 @@
 <?php
-$ch = curl_init('https://api.helius.xyz');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$response = curl_exec($ch);
-if (curl_errno($ch)) {
-    echo 'cURL error: ' . curl_error($ch);
-} else {
-    echo 'cURL works!';
-}
-curl_close($ch);
+include 'api-helper.php';
+$testData = callHeliusAPI('addresses/7xKXtg2CW87d97.../balances');
+var_dump($testData);
 ?>
