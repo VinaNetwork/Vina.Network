4d012beae7456fb9945671bbb29e3818b4ca6c1e
Vina Network <n9u93n@gmail.com>
2025-05-26 07:43:29 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,7 +2,13 @@
 // config.php
 
 // Định nghĩa hằng số cấu hình
-define('HELIUS_API_KEY', '8eb75cd9-015a-4e24-9de2-5be9ee0f1c63'); // API Key Helius
-define('ERROR_LOG_PATH', '/home/hthxhyqf/domains/vina.network/public_html/tools/error_log.txt'); // Đường dẫn log lỗi
-define('BASE_PATH', '/home/hthxhyqf/domains/vina.network/'); // Đường dẫn gốc của ứng dụng
+if (!defined('HELIUS_API_KEY')) {
+    define('HELIUS_API_KEY', '8eb75cd9-015a-4e24-9de2-5be9ee0f1c63'); // API Key Helius
+}
+if (!defined('ERROR_LOG_PATH')) {
+    define('ERROR_LOG_PATH', '/home/hthxhyqf/domains/vina.network/public_html/tools/error_log.txt'); // Đường dẫn log lỗi
+}
+if (!defined('BASE_PATH')) {
+    define('BASE_PATH', '/home/hthxhyqf/domains/vina.network/'); // Đường dẫn gốc của ứng dụng
+}
 ?>
