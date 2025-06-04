34aa33b80aa16ca81352d819c74eabc82434e99d
Vina Network <n9u93n@gmail.com>
2025-06-04 09:12:33 +0700

    Update api-helper.php

--- tools/api-helper.php
+++ tools/api-helper.php
@@ -73,7 +73,7 @@ function callSolscanAPI($endpoint, $params = []) {
     
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_HTTPHEADER, [
-        "Authorization: Bearer $api_key",
+        "token: $api_key", // Sửa từ Authorization: Bearer
         "Content-Type: application/json"
     ]);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
