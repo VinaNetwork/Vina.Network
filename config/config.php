47026e13ea8361adb13596f3bf8f56351726fa6d
Vina Network <n9u93n@gmail.com>
2025-06-08 13:42:33 +0700

    Add files via upload

--- config/config.php
+++ config/config.php
@@ -3,10 +3,10 @@
 
 // Định nghĩa hằng số cấu hình
 if (!defined('HELIUS_API_KEY')) {
-    define('HELIUS_API_KEY', 'Api_key_Helius'); // API Key Helius
+    define('HELIUS_API_KEY', '8eb75cd9-015a-4e24-9de2-5be9ee0f1c63'); // API Key Helius
 }
 if (!defined('SOLSCAN_API_KEY')) {
-    define('SOLSCAN_API_KEY', 'Api_Solana'); // API Key Solscan
+    define('SOLSCAN_API_KEY', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjcmVhdGVkQXQiOjE3NDgyNDcyMjI3OTgsImVtYWlsIjoibjl1OTNuQGdtYWlsLmNvbSIsImFjdGlvbiI6InRva2VuLWFwaSIsImFwaVZlcnNpb24iOiJ2MiIsImlhdCI6MTc0ODI0NzIyMn0.ukV8lKST8a1G46dA8rc3yu-CtZ90nxDI50o0q4xvgMk'); // API Key Solscan
 }
 if (!defined('ERROR_LOG_PATH')) {
     define('ERROR_LOG_PATH', '/var/www/vinanetwork/public_html/tools/error_log.txt'); // Đường dẫn log lỗi (đã sửa)
