fc595b8bd2e6dbe8792a0a8df8ee477e3602d41d
Vina Network <n9u93n@gmail.com>
2025-06-08 23:09:43 +0700

    Update navbar.php

--- include/navbar.php
+++ include/navbar.php
@@ -6,7 +6,9 @@
         </a>
     </div>
     <ul class="nav-1">
-        <li class="nav-2"><a href="https://www.vina.network/" class="nav-link"><i class="fas fa-home"></i> Home</a></li>
+        <li class="nav-2">
+            <a class="nav-link" href="https://www.vina.network/"><i class="fas fa-home"></i> Home</a>
+        </li>
         <li class="nav-2 dropdown">
             <a href="#" class="nav-link dropdown-toggle">
                 <i class="fas fa-box"></i> Product <i class="dropdown-icon fas fa-caret-down"></i>
