0250fdc43d68251d3dca2dbf8fa57a9093b6ec52
Vina Network <n9u93n@gmail.com>
2025-06-08 14:40:18 +0700

    Update navbar.php

--- include/navbar.php
+++ include/navbar.php
@@ -6,42 +6,42 @@
         </a>
     </div>
     <ul class="nav-links">
-        <li class="nav-1"><a href="https://www.vina.network/" class="nav-link"><i class="fas fa-home"></i> Home</a></li>
+        <li class="nav-1"><a href="https://www.vina.network/" class="nav-link"><i class="fas fa-home" aria-hidden="true"></i> Home</a></li>
         <li class="nav-1 dropdown">
             <a href="#" class="nav-link dropdown-toggle">
-                <i class="fas fa-box"></i> Product <i class="dropdown-icon fas fa-caret-down"></i>
+                <i class="fas fa-box" aria-hidden="true"></i> Product <i class="dropdown-icon fas fa-caret-down" aria-hidden="true"></i>
             </a>
             <ul class="dropdown-menu">
-                <li><a href="https://www.vina.network/notification" class="dropdown-item"><i class="fas fa-wallet"></i> Vina Wallet</a></li>
-                <li><a href="https://www.vina.network/notification" class="dropdown-item"><i class="fas fa-balance-scale"></i> Vina Stablecoin</a></li>
-                <li><a href="https://www.vina.network/notification" class="dropdown-item"><i class="fas fa-exchange-alt"></i> Vina Dex</a></li>
-                <li><a href="https://www.vina.network/notification" class="dropdown-item"><i class="fas fa-link"></i> Vina DeFi</a></li>
-                <li><a href="https://www.vina.network/notification" class="dropdown-item"><i class="fas fa-gamepad"></i> Vina GameFi</a></li>
-                <li><a href="https://www.vina.network/notification" class="dropdown-item"><i class="fas fa-image"></i> NFT Marketplace</a></li>
+                <li><a href="https://www.vina.network/notification" class="dropdown-item"><i class="fas fa-wallet" aria-hidden="true"></i> Vina Wallet</a></li>
+                <li><a href="https://www.vina.network/notification" class="dropdown-item"><i class="fas fa-balance-scale" aria-hidden="true"></i> Vina Stablecoin</a></li>
+                <li><a href="https://www.vina.network/notification" class="dropdown-item"><i class="fas fa-exchange-alt" aria-hidden="true"></i> Vina Dex</a></li>
+                <li><a href="https://www.vina.network/notification" class="dropdown-item"><i class="fas fa-link" aria-hidden="true"></i> Vina DeFi</a></li>
+                <li><a href="https://www.vina.network/notification" class="dropdown-item"><i class="fas fa-gamepad" aria-hidden="true"></i> Vina GameFi</a></li>
+                <li><a href="https://www.vina.network/notification" class="dropdown-item"><i class="fas fa-image" aria-hidden="true"></i> NFT Marketplace</a></li>
             </ul>
         </li>
         <li class="nav-1 dropdown">
             <a href="#" class="nav-link dropdown-toggle">
-                <i class="fas fa-screwdriver-wrench"></i> Tools <i class="dropdown-icon fas fa-caret-down"></i>
+                <i class="fas fa-screwdriver-wrench" aria-hidden="true"></i> Tools <i class="dropdown-icon fas fa-caret-down" aria-hidden="true"></i>
             </a>
             <ul class="dropdown-menu">
-                <li><a href="https://www.vina.network/tools/?tool=nft-holders" class="dropdown-item"><i class="fas fa-wallet"></i> NFT Holders</a></li>
-                <li><a href="https://www.vina.network/tools/?tool=nft-valuation" class="dropdown-item"><i class="fas fa-chart-line"></i> NFT Valuation</a></li>
-                <li><a href="https://www.vina.network/tools/?tool=nft-transactions" class="dropdown-item"><i class="fas fa-history"></i> NFT Transactions</a></li>
-                <li><a href="https://www.vina.network/tools/?tool=wallet-analysis" class="dropdown-item"><i class="fas fa-search"></i> Wallet Analysis</a></li>
+                <li><a href="https://www.vina.network/tools/?tool=nft-holders" class="dropdown-item"><i class="fas fa-wallet" aria-hidden="true"></i> NFT Holders</a></li>
+                <li><a href="https://www.vina.network/tools/?tool=nft-valuation" class="dropdown-item"><i class="fas fa-chart-line" aria-hidden="true"></i> NFT Valuation</a></li>
+                <li><a href="https://www.vina.network/tools/?tool=nft-transactions" class="dropdown-item"><i class="fas fa-history" aria-hidden="true"></i> NFT Transactions</a></li>
+                <li><a href="https://www.vina.network/tools/?tool=wallet-analysis" class="dropdown-item"><i class="fas fa-search" aria-hidden="true"></i> Wallet Analysis</a></li>
             </ul>
         </li>
         <li class="nav-1 dropdown">
             <a href="#" class="nav-link dropdown-toggle">
-                <i class="fas fa-coins"></i> Meme Coin <i class="dropdown-icon fas fa-caret-down"></i>
+                <i class="fas fa-coins" aria-hidden="true"></i> Meme Coin <i class="dropdown-icon fas fa-caret-down" aria-hidden="true"></i>
             </a>
             <ul class="dropdown-menu">
-                <li><a href="https://kimo.vina.network/token/" class="dropdown-item"><i class="fas fa-paw"></i> Kimo</a></li>
+                <li><a href="https://kimo.vina.network/token/" class="dropdown-item"><i class="fas fa-paw" aria-hidden="true"></i> Kimo</a></li>
             </ul>
         </li>
-        <li class="nav-1"><a href="https://www.vina.network/contact/" class="nav-link"><i class="fas fa-envelope"></i> Contact</a></li>
+        <li class="nav-1"><a href="https://www.vina.network/contact/" class="nav-link"><i class="fas fa-envelope" aria-hidden="true"></i> Contact</a></li>
     </ul>
-    <div class="burger" aria-label="Menu toggle">
+    <div class="burger" aria-label="Menu toggle" tabindex="0">
         <div class="line1"></div>
         <div class="line2"></div>
         <div class="line3"></div>
