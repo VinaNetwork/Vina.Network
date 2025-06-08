8c35e24fbf86a9949b0b065d3dfa56982b1b8421
Vina Network <n9u93n@gmail.com>
2025-06-08 20:04:15 +0700

    Update community.php

--- include/community.php
+++ include/community.php
@@ -1,9 +1,9 @@
 <!-- Join Our Community Section -->
-<section class="community-section">
-    <div class="community-content">
+<section class="community-1">
+    <div class="community-2">
         <h2 class="fade-in" data-delay="0">Join Our Community</h2>
         <p class="fade-in" data-delay="200">Be a part of the Vina Network revolution. Connect with us and stay updated!</p>
-        <div class="community-links fade-in" data-delay="400">
+        <div class="community-3 fade-in" data-delay="400">
             <a href="https://x.com/Vina_Network" target="_blank" class="community-link"><i class="fab fa-x-twitter"></i> Twitter</a>
             <a href="https://t.me/Vina_Network" target="_blank" class="community-link"><i class="fab fa-telegram"></i> Telegram</a>
             <a href="https://discord.gg/wm2H5epF" target="_blank" class="community-link"><i class="fab fa-discord"></i> Discord</a>
