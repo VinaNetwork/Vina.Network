72d11d71ceef111e6bcd649c4869d0ed6ab3e905
Vina Network <n9u93n@gmail.com>
2025-06-09 00:35:20 +0700

    Update index.php

--- notification/index.php
+++ notification/index.php
@@ -14,7 +14,7 @@ include '../include/header.php';
 
     <section class="n-1">
         <div class="n-2">
-            <h1><i class="fas fa-tools"></i></h1>
+            <i class="fas fa-tools"></i>
             <h1>Products Under Development</h1>
             <p>We’re sorry, but our products are currently under development. Our team is working hard to bring you the best experience. Stay tuned for updates!</p>
             <a href="https://www.vina.network/" class="cta-button">Back to Home</a>
