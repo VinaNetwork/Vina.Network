2e4e5f594c8dd1ea448f35aa1b561b93b777131f
Vina Network <n9u93n@gmail.com>
2025-06-09 09:15:39 +0700

    Update nft-holders.php

--- tools/nft-holders/nft-holders.php
+++ tools/nft-holders/nft-holders.php
@@ -128,38 +128,3 @@ error_log('nft-holders.php loaded at ' . date('Y-m-d H:i:s'));
 </div>
 
 <script src="../tools.js"></script>
-
-<?php
-function getNFTHolders($mintAddress, $offset = 0, $size = 50) {
-    $params = [
-        'groupKey' => 'collection',
-        'groupValue' => $mintAddress,
-        'page' => ceil(($offset + $size) / $size),
-        'limit' => $size
-    ];
-    
-    file_put_contents('/var/www/vinanetwork/public_html/tools/debug_log.txt', date('Y-m-d H:i:s') . " - Calling Helius API for holders - mintAddress: $mintAddress, offset: $offset, size: $size, page: {$params['page']}\n", FILE_APPEND);
-    error_log("nft-holders.php: Calling Helius API for holders - mintAddress: $mintAddress, offset: $offset, size: $size, page: {$params['page']} at " . date('Y-m-d H:i:s'));
-    
-    $data = callHeliusAPI('getAssetsByGroup', $params, 'POST');
-    
-    if (isset($data['error'])) {
-        error_log("nft-holders.php: getAssetsByGroup error - " . json_encode($data) . " at " . date('Y-m-d H:i:s'));
-        return ['error' => 'This is not an NFT collection address. Please enter a valid NFT Collection address.'];
-    }
-    
-    if (isset($data['result']['items']) && !empty($data['result']['items'])) {
-        $holders = array_map(function($item) {
-            return [
-                'owner' => $item['ownership']['owner'] ?? 'unknown',
-                'amount' => 1
-            ];
-        }, $data['result']['items']);
-        
-        return ['holders' => $holders];
-    }
-    
-    error_log("nft-holders.php: No holders found for address $mintAddress at " . date('Y-m-d H:i:s'));
-    return ['error' => 'This is not an NFT collection address. Please enter a valid NFT Collection address.'];
-}
-?>
