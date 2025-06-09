3c9fcfe7a5e7c9140be57ad811fb3d8181583fd9
Vina Network <n9u93n@gmail.com>
2025-06-09 12:05:47 +0700

    Update load-tool.php

--- tools/load-tool.php
+++ tools/load-tool.php
@@ -1,8 +1,4 @@
 <?php
-// Điều kiện để truy cập config.php
-define('VINANETWORK_ENTRY', true);
-require_once '../config/config.php';
-
 // Cấu hình log lỗi
 $config_path = '../config/config.php';
 if (!file_exists($config_path)) {
