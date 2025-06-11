7b29ecac0dc9af03bf7cb5a344452d3087ffc853
Vina Network <n9u93n@gmail.com>
2025-06-11 14:50:58 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 // config/config.php
 if (!defined('VINANETWORK_STATUS')) {
     http_response_code(403);
-    exit('No direct script access allowed!');
+    exit('No direct access allowed!');
 }
 
 // Định nghĩa hằng số cấu hình
@@ -10,9 +10,8 @@ if (!defined('HELIUS_API_KEY')) {
     define('HELIUS_API_KEY', '8eb75cd9-015a-4e24-9de2-5be9ee0f1c63');
 }
 if (!defined('ERROR_LOG_PATH')) {
-    define('ERROR_LOG_PATH', TOOLS_PATH . 'error_log.txt'); // Sử dụng TOOLS_PATH từ bootstrap.php
+    define('ERROR_LOG_PATH', TOOLS_PATH . 'error_log.txt');
 }
 if (!defined('BASE_PATH')) {
-    define('BASE_PATH', ROOT_PATH); // Sử dụng ROOT_PATH từ bootstrap.php
+    define('BASE_PATH', ROOT_PATH);
 }
-?>
