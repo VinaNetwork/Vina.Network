de6fa44c01a6b7fb7625c113dd55966b861ba60c
Vina Network <n9u93n@gmail.com>
2025-06-12 09:03:10 +0700

    Update index.php

--- tools/index.php
+++ tools/index.php
@@ -1,11 +1,7 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 ob_start();
 define('VINANETWORK', true);
-require_once '../bootstrap.php';
+require_once 'bootstrap.php';
 ini_set('log_errors', 1);
 ini_set('error_log', ERROR_LOG_PATH);
 ini_set('display_errors', 0);
