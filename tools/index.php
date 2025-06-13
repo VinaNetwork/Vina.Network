005f3d3f61eec8b2f10f517260f1d74c1edae80b
Vina Network <n9u93n@gmail.com>
2025-06-13 15:58:37 +0700

    Update index.php

--- tools/index.php
+++ tools/index.php
@@ -110,9 +110,7 @@ include $header_path;
     }
     </script>
 
-    <script>
-        console.log('Attempting to load JS files...');
-    </script>
+    <script>console.log('Attempting to load JS files...');</script>
     <script src="../js/vina.js?t=<?php echo time(); ?>" onerror="console.error('Failed to load js/vina.js')"></script>
     <script src="../js/navbar.js?t=<?php echo time(); ?>" onerror="console.error('Failed to load js/navbar.js')"></script>
     <script src="../js/chart.js?t=<?php echo time(); ?>" onerror="console.error('Failed to load js/chart.js')"></script>
