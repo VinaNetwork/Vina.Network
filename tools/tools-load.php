25292ad874ba863cf39956c2f15ae45f72304782
Vina Network <n9u93n@gmail.com>
2025-06-13 18:37:42 +0700

    Update tools-load.php

--- tools/tools-load.php
+++ tools/tools-load.php
@@ -39,7 +39,7 @@ if (!in_array($tool, $valid_tools)) {
 $tool_files = [
     'nft-holders' => 'nft-holders/nft-holders.php',
     'nft-valuation' => 'nft-valuation/nft-valuation.php',
-    'transaction' => 'transaction/transaction.php',
+    'transaction' => 'nft-transaction/transaction.php',
     'wallet-analysis' => 'wallet-analysis/wallet-analysis.php'
 ];
 
