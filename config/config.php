e83d658ef2caebd0c9e0a25322ef0d155347e35a
Vina Network <n9u93n@gmail.com>
2025-06-14 20:11:03 +0700

    Add files via upload

--- config/config.php
+++ config/config.php
@@ -8,15 +8,13 @@ if (!defined('VINANETWORK_ENTRY')) {
 
 // API HELIUS
 if (!defined('HELIUS_API_KEY')) {
-    define('HELIUS_API_KEY', 'YOUR_HELIUS_API'); // API Key Helius
+    define('HELIUS_API_KEY', '8eb75cd9-015a-4e24-9de2-5be9ee0f1c63'); // API Key Helius
 }
 
-// Error Log
+// Folder Error
 if (!defined('ERROR_LOG_PATH')) {
     define('ERROR_LOG_PATH', '/var/www/vinanetwork/public_html/tools/error_log.txt');
 }
-
-// Base Path
 if (!defined('BASE_PATH')) {
     define('BASE_PATH', '/var/www/vinanetwork/public_html/');
 }
