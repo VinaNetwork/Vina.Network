dd7c2a0f506ef1274858dbae27b9457e3e064011
Vina Network <n9u93n@gmail.com>
2025-06-14 10:56:48 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,15 +1,17 @@
 <?php
 // config.php
-// Yêu cầu bắt buộc muốn include file config.php thì phải có VINANETWORK_ENTRY
+// Điều kiện truy cập config.php
 if (!defined('VINANETWORK_ENTRY')) {
     http_response_code(403);
     exit('No direct script access allowed!');
 }
 
-// Định nghĩa hằng số cấu hình
+// API HELIUS
 if (!defined('HELIUS_API_KEY')) {
     define('HELIUS_API_KEY', '8eb75cd9-015a-4e24-9de2-5be9ee0f1c63'); // API Key Helius
 }
+
+// Folder Error
 if (!defined('ERROR_LOG_PATH')) {
     define('ERROR_LOG_PATH', '/var/www/vinanetwork/public_html/tools/error_log.txt');
 }
