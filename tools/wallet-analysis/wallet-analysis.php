d8f3635c25300e912c5d74b6065433f82f3e83fe
Vina Network <n9u93n@gmail.com>
2025-06-21 18:28:06 +0700

    Update wallet-analysis.php

--- tools/wallet-analysis/wallet-analysis.php
+++ tools/wallet-analysis/wallet-analysis.php
@@ -212,10 +212,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['walletAddress'])) {
                                 <span><?php echo substr(htmlspecialchars($nft['mint']), 0, 4) . '...' . substr(htmlspecialchars($nft['mint']), -4); ?></span>
                                 <i class="fas fa-copy copy-icon" title="Copy full address" data-full="<?php echo htmlspecialchars($nft['mint']); ?>"></i>
                             </td>
-                            <td class="address-cell">
-                                <span><?php echo substr(htmlspecialchars($nft['collection']), 0, 4) . '...' . substr(htmlspecialchars($nft['collection']), -4); ?></span>
-                                <i class="fas fa-copy copy-icon" title="Copy full address" data-full="<?php echo htmlspecialchars($nft['collection']); ?>"></i>
-                            </td>
+                            <td class="address-cell"><?php echo htmlspecialchars($nft['collection']); ?></td>
                         </tr>
                         <?php endforeach; ?>
                     </table>
