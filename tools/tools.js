fb16e132792748e6e662566bbfb9e0cd2e1f33bf
Vina Network <n9u93n@gmail.com>
2025-06-25 20:50:58 +0700

    Update tools.js

--- tools/tools.js
+++ tools/tools.js
@@ -1,8 +1,7 @@
 // ============================================================================
 // File: tools/tools.js
 // Description: Script for managing the entire tool page, including tool tab navigation, wallet analysis tab navigation, form submission, export, and copy functionality.
-// Author: Vina Network
-// Version: Updated for slide animation and back button functionality
+// Created by: Vina Network
 // ============================================================================
 
 document.addEventListener('DOMContentLoaded', () => {
