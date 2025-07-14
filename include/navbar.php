78eeaa53fba191d90bc7b1f01a49f21813b50294
Vina Network <n9u93n@gmail.com>
2025-07-14 17:28:58 +0700

    Update navbar.php

--- include/navbar.php
+++ include/navbar.php
@@ -9,55 +9,59 @@
 <!-- Main Navigation Bar -->
 <nav class="navbar" role="navigation" aria-label="Main navigation">
     <div class="logo">
-        <a href="https://www.vina.network/" aria-label="Vina Network Home">
-            <img src="https://www.vina.network/img/logo.png" alt="Vina Network Logo">
+        <a href="/" aria-label="Vina Network Home">
+            <img src="/img/logo.png" alt="Vina Network Logo">
         </a>
     </div>
 
     <!-- Navigation Links -->
     <ul class="navbar-content">
         <li class="navbar-item">
-            <a href="https://www.vina.network/" class="navbar-link">
+            <a href="/" class="navbar-link">
                 <i class="fas fa-home"></i> Home
             </a>
         </li>
+
         <!-- Product Dropdown -->
         <li class="navbar-item dropdown">
             <a href="#" class="navbar-link dropdown-toggle">
                 <i class="fas fa-box"></i> Product <i class="dropdown-icon fas fa-caret-down"></i>
             </a>
             <ul class="dropdown-menu">
-                <li><a href="https://www.vina.network/notification" class="dropdown-link"><i class="fas fa-wallet"></i> Vina Wallet</a></li>
-                <li><a href="https://www.vina.network/notification" class="dropdown-link"><i class="fas fa-balance-scale"></i> Vina Stablecoin</a></li>
-                <li><a href="https://www.vina.network/notification" class="dropdown-link"><i class="fas fa-exchange-alt"></i> Vina Dex</a></li>
-                <li><a href="https://www.vina.network/notification" class="dropdown-link"><i class="fas fa-link"></i> Vina DeFi</a></li>
-                <li><a href="https://www.vina.network/notification" class="dropdown-link"><i class="fas fa-gamepad"></i> Vina GameFi</a></li>
-                <li><a href="https://www.vina.network/notification" class="dropdown-link"><i class="fas fa-image"></i> NFT Marketplace</a></li>
+                <li><a href="/notification" class="dropdown-link"><i class="fas fa-wallet"></i> Vina Wallet</a></li>
+                <li><a href="/notification" class="dropdown-link"><i class="fas fa-balance-scale"></i> Vina Stablecoin</a></li>
+                <li><a href="/notification" class="dropdown-link"><i class="fas fa-exchange-alt"></i> Vina Dex</a></li>
+                <li><a href="/notification" class="dropdown-link"><i class="fas fa-link"></i> Vina DeFi</a></li>
+                <li><a href="/notification" class="dropdown-link"><i class="fas fa-gamepad"></i> Vina GameFi</a></li>
+                <li><a href="/notification" class="dropdown-link"><i class="fas fa-image"></i> NFT Marketplace</a></li>
             </ul>
         </li>
+
         <!-- Tools Dropdown -->
         <li class="navbar-item dropdown">
             <a href="#" class="navbar-link dropdown-toggle">
                 <i class="fas fa-screwdriver-wrench"></i> Tools <i class="dropdown-icon fas fa-caret-down"></i>
             </a>
             <ul class="dropdown-menu">
-                <li><a href="https://www.vina.network/tools/?tool=nft-info" class="dropdown-link"><i class="fas fa-image"></i> Check NFT Info</a></li>
-                <li><a href="https://www.vina.network/tools/?tool=nft-holders" class="dropdown-link"><i class="fas fa-wallet"></i> Check NFT Holders</a></li>
-                <li><a href="https://www.vina.network/tools/?tool=wallet-creators" class="dropdown-link"><i class="fas fa-image"></i> Check Wallet Creators</a></li>
-                <li><a href="https://www.vina.network/tools/?tool=wallet-analysis" class="dropdown-link"><i class="fas fa-chart-line"></i> Wallet Analysis</a></li>
+                <li><a href="/tools/?tool=nft-info" class="dropdown-link"><i class="fas fa-image"></i> Check NFT Info</a></li>
+                <li><a href="/tools/?tool=nft-holders" class="dropdown-link"><i class="fas fa-wallet"></i> Check NFT Holders</a></li>
+                <li><a href="/tools/?tool=wallet-creators" class="dropdown-link"><i class="fas fa-image"></i> Check Wallet Creators</a></li>
+                <li><a href="/tools/?tool=wallet-analysis" class="dropdown-link"><i class="fas fa-chart-line"></i> Wallet Analysis</a></li>
             </ul>
         </li>
+
         <!-- Meme Coin Dropdown -->
         <li class="navbar-item dropdown">
             <a href="#" class="navbar-link dropdown-toggle">
                 <i class="fas fa-coins"></i> Meme Coin <i class="dropdown-icon fas fa-caret-down"></i>
             </a>
             <ul class="dropdown-menu">
-                <li><a href="https://www.vina.network/notification/" class="dropdown-link"><i class="fa-solid fa-crown"></i> Kimo</a></li>
+                <li><a href="/notification/" class="dropdown-link"><i class="fa-solid fa-crown"></i> Kimo</a></li>
             </ul>
         </li>
+
         <li class="navbar-item">
-            <a href="https://www.vina.network/contact/" class="navbar-link">
+            <a href="/contact/" class="navbar-link">
                 <i class="fas fa-envelope"></i> Contact
             </a>
         </li>
