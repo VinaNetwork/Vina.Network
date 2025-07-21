278d86d7cf44ea007f87b535df0b27ddc6e39759
Vina Network <n9u93n@gmail.com>
2025-07-21 08:38:14 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -37,13 +37,4 @@ if (!defined('DB_PASS')) {
 if (!defined('HELIUS_API_KEY')) {
     define('HELIUS_API_KEY', '8eb75cd9-015a-4e24-9de2-5be9ee0f1c63');
 }
-
-// Logging and Path Configuration
-if (!defined('ERROR_LOG_PATH')) {
-    define('ERROR_LOG_PATH', '/var/www/vinanetwork/public_html/tools/error_log.txt');
-}
-
-if (!defined('BASE_PATH')) {
-    define('BASE_PATH', '/var/www/vinanetwork/public_html/');
-}
 ?>
