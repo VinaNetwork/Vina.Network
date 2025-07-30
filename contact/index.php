e63dd0d9308a2210de8a22addd78f2d9e1b6c761
Vina Network <n9u93n@gmail.com>
2025-07-30 08:59:34 +0700

    Update index.php

--- contact/index.php
+++ contact/index.php
@@ -10,7 +10,7 @@
 if (!defined('VINANETWORK_ENTRY')) {
     define('VINANETWORK_ENTRY', true);
 }
-require_once __DIR__ . '/config/bootstrap.php';
+require_once __DIR__ . '/../config/bootstrap.php';
 
 // Head Section (Meta, Styles, Title) is included via header.php
 $page_title = "Contact Us - Vina Network";
