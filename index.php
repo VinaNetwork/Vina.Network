8ec3bfdf9562a180ed24dd7ee1adeabe46a431b4
Vina Network <n9u93n@gmail.com>
2025-08-03 17:06:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php
 // ============================================================================
 // File: index.php
 // Description: Homepage of Vina Network.
