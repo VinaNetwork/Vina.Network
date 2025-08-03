2813a950f6014abf4452e74d45e88e219667093c
Vina Network <n9u93n@gmail.com>
2025-08-03 16:13:39 +0700

    Update status.php

--- make-market/process/status.php
+++ make-market/process/status.php
@@ -10,7 +10,7 @@ if (!defined('VINANETWORK_ENTRY')) {
     define('VINANETWORK_ENTRY', true);
 }
 
-$root_path = '../';
+$root_path = '../../';
 require_once $root_path . 'config/bootstrap.php';
 require_once $root_path . 'config/config.php';
 
