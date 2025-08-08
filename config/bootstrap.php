12462b76f7996dd5321cc3e55bd569ce710028fa
Vina Network <n9u93n@gmail.com>
2025-08-08 10:39:44 +0700

    Update bootstrap.php

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -40,7 +40,6 @@ define('ENVIRONMENT', 'development');
 ini_set('display_errors', 0);
 ini_set('display_startup_errors', 0);
 error_reporting(E_ALL);
-session_start();
 ini_set('log_errors', true);
 ini_set('error_log', ERROR_LOG_PATH);
 
