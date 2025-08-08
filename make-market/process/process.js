eedb31a86dffbfa5176b68b10558c20ea3c2736e
Vina Network <n9u93n@gmail.com>
2025-08-08 20:41:48 +0700

    Update process.js

--- make-market/process/process.js
+++ make-market/process/process.js
@@ -5,7 +5,7 @@
 // ============================================================================
 
 // Import auth utilities
-import { initializeAuth, addAuthHeaders, addAxiosAuthHeaders } from '/make-market/auth.js';
+import { initializeAuth, addAuthHeaders, addAxiosAuthHeaders } from '/make-market/process/auth.js';
 
 // Log message function
 function log_message(message, log_file = 'make-market.log', module = 'make-market', log_type = 'INFO') {
