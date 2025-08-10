81345433e3a4a910c2a969c274a8f8713f5e8260
Vina Network <n9u93n@gmail.com>
2025-08-10 15:34:54 +0700

    Update index.php

--- make-market/index.php
+++ make-market/index.php
@@ -466,8 +466,8 @@ $defaultSlippage = 0.5;
 <!-- Scripts - Source code -->
 <script defer src="/js/vina.js?t=<?php echo time(); ?>" onerror="console.error('Failed to load /js/vina.js')"></script>
 <script defer src="/js/navbar.js?t=<?php echo time(); ?>" onerror="console.error('Failed to load /js/navbar.js')"></script>
-<script defer src="/make-market/mm.js?t=<?php echo time(); ?>" onerror="console.error('Failed to load mm.js')"></script>
-<script defer src="/make-market/ui.js?t=<?php echo time(); ?>" onerror="console.error('Failed to load mm.js')"></script>
+<script defer src="/make-market/js/mm.js?t=<?php echo time(); ?>" onerror="console.error('Failed to load mm.js')"></script>
+<script defer src="/make-market/js/ui.js?t=<?php echo time(); ?>" onerror="console.error('Failed to load mm.js')"></script>
 </body>
 </html>
 <?php ob_end_flush(); ?>
