7e88a69e52ebe121de48c47b8c68e27d725921c8
Vina Network <n9u93n@gmail.com>
2025-08-10 14:37:24 +0700

    Update get-csrf.php

--- make-market/process/get-csrf.php
+++ make-market/process/get-csrf.php
@@ -11,7 +11,7 @@ if (!defined('VINANETWORK_ENTRY')) {
 
 $root_path = __DIR__ . '/../../';
 require_once $root_path . 'config/bootstrap.php';
-require_once $root_path . 'make-market/process/auth.php';
+require_once $root_path . 'make-market/security/auth.php';
 
 // Start output buffering
 ob_start();
