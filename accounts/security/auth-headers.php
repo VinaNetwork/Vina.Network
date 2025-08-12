973bdcfec5a316f45ff3c31814bc75753fd176ba
Vina Network <n9u93n@gmail.com>
2025-08-12 08:43:51 +0700

    Update auth-headers.php

--- accounts/security/auth-headers.php
+++ accounts/security/auth-headers.php
@@ -18,7 +18,7 @@ header("Referrer-Policy: strict-origin-when-cross-origin");
 // Content Security Policy - allow Phantom Wallet and site scripts
 header("Content-Security-Policy: "
     . "default-src 'self'; "
-    . "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net https://unpkg.com https://www.googletagmanager.com; "
+    . "script-src 'self' https://cdn.jsdelivr.net https://unpkg.com https://www.googletagmanager.com; "
     . "connect-src 'self' $csp_base https://*.phantom.app; https://www.google-analytics.com"
     . "img-src 'self' $csp_base data: https:; "
     . "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; "
