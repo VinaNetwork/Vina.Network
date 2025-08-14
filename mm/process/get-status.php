c8248684b171b3724346d8cd38a1b6ee9edd360e
Vina Network <n9u93n@gmail.com>
2025-08-14 20:55:39 +0700

    Update get-status.php

--- mm/process/get-status.php
+++ mm/process/get-status.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 // ============================================================================
 // File: mm/process/get-status.php
 // Description: Update transaction status and error in make_market table
