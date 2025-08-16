8b8b5487cf673f4db316a956c8196aa3748a1e8d
Vina Network <n9u93n@gmail.com>
2025-08-16 12:08:43 +0700

    Update bootstrap.php

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -14,14 +14,11 @@ if (!defined('VINANETWORK_ENTRY')) {
 // Website root directory
 define('ROOT_PATH', dirname(__DIR__) . '/');
 // Load configuration
+require_once ROOT_PATH . 'config/constants.php';
+require_once ROOT_PATH . 'config/logging.php';
 require_once ROOT_PATH . 'config/config.php';
+require_once ROOT_PATH . 'config/session.php';
 require_once ROOT_PATH . 'config/csrf.php';
 require_once ROOT_PATH . 'config/db.php';
-
-// PHP configuration
-ini_set('display_errors', 0);
-ini_set('display_startup_errors', 0);
-error_reporting(E_ALL);
-ini_set('log_errors', true);
-ini_set('error_log', ERROR_LOG_PATH);
+require_once ROOT_PATH . 'config/error.php';
 ?>
