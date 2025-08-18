46a0da78541115f47b7e33ec4bfe30339e34785e
Vina Network <n9u93n@gmail.com>
2025-08-18 08:34:59 +0700

    Update and rename mm/get-network.php to mm/network/get-network.php

--- mm/network/get-network.php
+++ mm/network/get-network.php
@@ -1,6 +1,6 @@
 <?php
 // ============================================================================
-// File: mm/get-network.php
+// File: mm/network/get-network.php
 // Description: API endpoint to return SOLANA_NETWORK value
 // Created by: Vina Network
 // ============================================================================
@@ -9,9 +9,9 @@ if (!defined('VINANETWORK_ENTRY')) {
     define('VINANETWORK_ENTRY', true);
 }
 
-$root_path = __DIR__ . '/../';
+$root_path = __DIR__ . '/../../';
 require_once $root_path . 'config/bootstrap.php';
-require_once $root_path . 'mm/network.php';
+require_once $root_path . 'mm/network/network.php';
 
 // Protect endpoint with CSRF
 csrf_protect();
