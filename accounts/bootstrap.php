5e463c687560b272586ce97775812b287f762236
Vina Network <n9u93n@gmail.com>
2025-08-19 00:39:21 +0700

    Update bootstrap.php

--- accounts/bootstrap.php
+++ accounts/bootstrap.php
@@ -13,12 +13,12 @@ if (!defined('VINANETWORK_ENTRY')) {
 
 $root_path = __DIR__ . '/../';
 // General configuration
-require_once $root_path . 'config/constants.php'; 		  // Dynamic Domain Name Definition
-require_once $root_path . 'config/logging.php'; 		    // Logging utilities
-require_once $root_path . 'config/config.php'; 			    // Central configuration
-require_once $root_path . 'config/error.php'; 			    // PHP configuration
-require_once $root_path . 'config/session.php'; 		    // Initialize session with security options
-require_once $root_path . 'config/db.php'; 				      // Database connection management
+require_once $root_path . 'core/constants.php'; 		  // Dynamic Domain Name Definition
+require_once $root_path . 'core/logging.php'; 		    // Logging utilities
+require_once $root_path . 'core/config.php'; 			    // Central configuration
+require_once $root_path . 'core/error.php'; 			    // PHP configuration
+require_once $root_path . 'core/session.php'; 		    // Initialize session with security options
+require_once $root_path . 'core/db.php'; 				      // Database connection management
 // Custom configuration
 require_once $root_path . 'accounts/header-auth.php'; 	// Security Headers
 require_once $root_path . 'accounts/csrf/csrf.php'; 	  // CSRF Token
