f969a4eed10b6ab6e4f218150b0f9e0524fa14dc
Vina Network <n9u93n@gmail.com>
2025-08-18 16:22:14 +0700

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -17,4 +17,11 @@ $is_secure = $protocol === 'https://';
 $domain = filter_var($_SERVER['HTTP_HOST'], FILTER_SANITIZE_URL) ?: 'localhost'; // Sanitize HTTP_HOST
 define('BASE_URL', $protocol . $domain . '/');
 $csp_base = rtrim(BASE_URL, '/');
+
+// List of allowed sources
+define('ALLOWED_ORIGINS', [
+    BASE_URL,
+    'http://localhost',
+    'http://localhost:8080'
+]);
 ?>
