34818b82fda4cd97e48b7f89d4db30d0d3968d9b
Vina Network <n9u93n@gmail.com>
2025-08-18 20:30:02 +0700

    Update get-decimals.php

--- mm/process/get-decimals.php
+++ mm/process/get-decimals.php
@@ -12,6 +12,7 @@ if (!defined('VINANETWORK_ENTRY')) {
 $root_path = __DIR__ . '/../../';
 require_once $root_path . 'config/bootstrap.php';
 require_once $root_path . 'mm/header-auth.php';
+require_once $root_path . 'mm/csrf/csrf.php';
 
 // Start session if not already started
 if (session_status() === PHP_SESSION_NONE) {
