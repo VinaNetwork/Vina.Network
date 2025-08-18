d2529fcacf9ac6ecc1fb9d48b7bf2e0e8cebd5d1
Vina Network <n9u93n@gmail.com>
2025-08-18 20:31:12 +0700

    Update bootstrap.php

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -16,6 +16,5 @@ require_once __DIR__ . '/logging.php';
 require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/error.php';
 require_once __DIR__ . '/session.php';
-require_once __DIR__ . '/csrf.php';
 require_once __DIR__ . '/db.php';
 ?>
