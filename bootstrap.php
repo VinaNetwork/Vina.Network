f4bd5d7913b02c0237b5a098bb606e1d5739a44f
Vina Network <n9u93n@gmail.com>
2025-08-19 15:04:13 +0700

    Create bootstrap.php

---
+++ bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+// ============================================================================
+// File: bootstrap.php
+// Description: Security check and utility functions for Accounts modules
+// Created by: Vina Network
+// ============================================================================
+
+// Access Conditions
+if (!defined('VINANETWORK_ENTRY')) {
+    http_response_code(403);
+    exit('No direct access allowed!');
+}
+
+$root_path = __DIR__ . '/';
+// General configuration
+require_once $root_path . 'core/constants.php'; 		  // Dynamic Domain Name Definition
+require_once $root_path . 'core/logging.php'; 		    // Logging utilities
+require_once $root_path . 'core/error.php'; 			    // PHP configuration
+?>
