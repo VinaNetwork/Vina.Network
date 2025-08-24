082adde97fc776152d5c549514819b19cf0e6483
Vina Network <n9u93n@gmail.com>
2025-08-25 00:14:43 +0700

    Update bootstrap.php

--- mm/bootstrap.php
+++ mm/bootstrap.php
@@ -13,15 +13,15 @@ if (!defined('VINANETWORK_ENTRY')) {
 
 $root_path = __DIR__ . '/../';
 // Core configuration
-require_once $root_path . 'core/constants.php'; 		// Dynamic Domain Name Definition
-require_once $root_path . 'core/logging.php'; 		  // Logging utilities
+require_once $root_path . 'core/constants.php'; 		  // Dynamic Domain Name Definition
+require_once $root_path . 'core/logging.php'; 		      // Logging utilities
 require_once $root_path . 'core/config.php'; 			  // Central configuration
 require_once $root_path . 'core/error.php'; 			  // PHP configuration
-require_once $root_path . 'core/session.php'; 		  // Initialize session with security options
-require_once $root_path . 'core/db.php'; 				    // Database connection management
+require_once $root_path . 'core/session.php'; 		      // Initialize session with security options
+require_once $root_path . 'core/db.php'; 				  // Database connection management
 // Custom configuration
 require_once $root_path . 'mm/header-auth.php'; 		  // Security Headers
-require_once $root_path . 'mm/network/network.php'; 	// Devnet | Testnet | Mainnet
+require_once $root_path . 'mm/network/network.php'; 	  // Devnet | Testnet | Mainnet
 require_once $root_path . 'mm/csrf/csrf.php'; 			  // CSRF Token
-require_once $root_path . '../vendor/autoload.php'; 	// Solana Library
+require_once $root_path . '../vendor/autoload.php'; 	  // Solana Library
 ?>
