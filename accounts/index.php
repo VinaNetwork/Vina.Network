ca180bd5c24cb14da61000953adfedd8ea627518
Vina Network <n9u93n@gmail.com>
2025-08-31 22:35:00 +0700

    Update index.php

--- accounts/index.php
+++ accounts/index.php
@@ -12,7 +12,7 @@ if (!defined('VINANETWORK_ENTRY')) {
 
 $root_path = __DIR__ . '/../';
 // constants | logging | config | error | session | database | header-auth.php | csrf.php | wallet-auth.php
-require_once $root_path . 'accounts/bootstrap.php';
+require_once $root_path . 'core/bootstrap.php';
 
 // Protect POST requests with CSRF
 csrf_protect();
