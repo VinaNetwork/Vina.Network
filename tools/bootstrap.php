d3ffe7fc76a6485ae71ea0e864153ee97c97ea4d
Vina Network <n9u93n@gmail.com>
2025-09-01 03:24:33 +0700

    Update bootstrap.php

--- tools/bootstrap.php
+++ tools/bootstrap.php
@@ -7,20 +7,14 @@
 
 // Access Conditions
 if (!defined('VINANETWORK_ENTRY')) {
-    http_response_code(403);
-    exit('No direct access allowed!');
+    define('VINANETWORK_ENTRY', true);
 }
 
 $root_path = __DIR__ . '/../';
-// General configuration
+// Configuration
 require_once $root_path . 'core/constants.php'; 		  // Dynamic Domain Name Definition
-require_once $root_path . 'core/logging.php'; 		      // Logging utilities
+require_once $root_path . 'core/logs/logging.php'; 		  // Logging utilities
 require_once $root_path . 'core/config.php'; 			  // Central configuration
 require_once $root_path . 'core/error.php'; 			  // PHP configuration
-require_once $root_path . 'core/session.php'; 		      // Initialize session with security options
-
-// Custom configuration
-// require_once $root_path . 'tools/core/tools-load.php'; 	  // Load file
-// require_once $root_path . 'tools/core/tools-api.php'; 	  // API handles
 require_once $root_path . 'tools/core/csrf.php'; 	      // CSRF Token
 ?>
