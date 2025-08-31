99b5e6d77a3a126f68d595763eae727e7e55a7c1
Vina Network <n9u93n@gmail.com>
2025-09-01 02:22:19 +0700

    Update logging.php

--- core/logs/logging.php
+++ core/logs/logging.php
@@ -5,8 +5,8 @@
 // Created by: Vina Network
 // ============================================================================
 
-// Access Conditions - allow includes from other files
-if (!defined('VINANETWORK_ENTRY') && php_sapi_name() !== 'cli') {
+// Access Conditions
+if (!defined('VINANETWORK_ENTRY')) {
     http_response_code(403);
     exit('No direct access allowed!');
 }
