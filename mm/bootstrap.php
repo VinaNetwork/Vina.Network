fa4d123efba86cda20e05703ab6f76e276f5d7e8
Vina Network <n9u93n@gmail.com>
2025-09-01 23:21:53 +0700

    Create bootstrap.php

---
+++ mm/bootstrap.php
@@ -0,0 +1,25 @@
+<?php
+// ============================================================================
+// File: mm/bootstrap.php
+// Description: Security check and utility functions for Make Market modules
+// Created by: Vina Network
+// ============================================================================
+
+// Access Conditions
+if (!defined('VINANETWORK_ENTRY')) {
+    define('VINANETWORK_ENTRY', true);
+}
+
+// General configuration
+require_once __DIR__ . '/../core/constants.php'; 		    // Dynamic Domain Name Definition
+require_once __DIR__ . '/../core/logging.php'; 	        // Logging utilities
+require_once __DIR__ . '/../core/config.php'; 			    // Central configuration
+require_once __DIR__ . '/../core/error.php'; 			      // PHP configuration
+require_once __DIR__ . '/../core/session.php'; 		      // Initialize session with security options
+require_once __DIR__ . '/../core/db.php'; 				      // Database connection management
+require_once __DIR__ . '/../core/header-auth.php'; 		  // Security Headers
+require_once __DIR__ . '/../../vendor/autoload.php'; 	  // Solana Library
+
+// Custom configuration
+require_once __DIR__ . '/network/network.php';          // Network configuration
+?>
