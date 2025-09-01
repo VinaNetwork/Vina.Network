67dc647ea5fc31f5c8c011b9e982c0b295994e0a
Vina Network <n9u93n@gmail.com>
2025-09-01 12:20:04 +0700

    Update logout.php

--- accounts/logout.php
+++ accounts/logout.php
@@ -6,6 +6,15 @@
 // ============================================================================
 
 ob_start();
+// Access Conditions
+if (!defined('VINANETWORK_ENTRY')) {
+    define('VINANETWORK_ENTRY', true);
+}
+
+$root_path = __DIR__ . '/../';
+// constants | logging | config | error | session | database | header-auth | csrf | wallet-auth
+require_once $root_path . 'accounts/bootstrap.php';
+
 // Ensure POST request and AJAX
 if ($_SERVER['REQUEST_METHOD'] !== 'POST' || 
     !isset($_SERVER['HTTP_X_REQUESTED_WITH']) || 
