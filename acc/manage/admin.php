227172623842ad9be3f7b5b1cb416b2ca7b5f10f
Vina Network <n9u93n@gmail.com>
2025-09-06 12:41:04 +0700

    Update admin.php

--- acc/manage/admin.php
+++ acc/manage/admin.php
@@ -90,7 +90,7 @@ try {
 $page_title = "Admin - Manage Accounts";
 $page_description = "Admin panel to manage user accounts on Vina Network";
 $page_keywords = "Vina Network, admin, manage accounts";
-$page_url = BASE_URL . "acc/manage/admin";
+$page_url = BASE_URL . "acc/admin";
 $page_css = ['/acc/acc.css', '/acc/manage/admin.css'];
 ?>
 
