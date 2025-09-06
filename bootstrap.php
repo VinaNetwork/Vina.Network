6bc8d2c404aef3b6ab9166fab4c1988e6435307d
Vina Network <n9u93n@gmail.com>
2025-09-06 15:37:58 +0700

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -7,13 +7,15 @@
 
 // Access Conditions
 if (!defined('VINANETWORK_ENTRY')) {
-    http_response_code(403);
-    exit('No direct access allowed!');
+    define('VINANETWORK_ENTRY', true);
 }
 
 $root_path = __DIR__ . '/';
 // General configuration
 require_once $root_path . 'core/constants.php'; 		  // Dynamic Domain Name Definition
-require_once $root_path . 'core/logging.php'; 		    // Logging utilities
-require_once $root_path . 'core/error.php'; 			    // PHP configuration
+require_once $root_path . 'core/logging.php'; 	          // Logging utilities
+require_once $root_path . 'core/config.php'; 			  // Central configuration
+require_once $root_path . 'core/error.php'; 			  // PHP configuration
+require_once $root_path . 'core/session.php'; 		      // Initialize session with security options
+require_once $root_path . 'core/db.php'; 				  // Database connection management
 ?>
