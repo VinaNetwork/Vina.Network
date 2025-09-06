cd6f61d31e05350ee3085c8fead33fb9a15ffb85
Vina Network <n9u93n@gmail.com>
2025-09-06 16:39:56 +0700

    Update get-logs.php

--- mm/core/get-logs.php
+++ mm/core/get-logs.php
@@ -5,10 +5,6 @@
 // Created by: Vina Network
 // ============================================================================
 
-if (!defined('VINANETWORK_ENTRY')) {
-    define('VINANETWORK_ENTRY', true);
-}
-
 $root_path = __DIR__ . '/../../';
 require_once $root_path . 'acc/bootstrap.php';
 
