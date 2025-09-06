f5d44b009c10bf7d91583e9b3eaddbfec0180cae
Vina Network <n9u93n@gmail.com>
2025-09-06 16:24:34 +0700

    Update disconnect.php

--- acc/disconnect.php
+++ acc/disconnect.php
@@ -6,11 +6,8 @@
 // ============================================================================
 
 ob_start();
-if (!defined('VINANETWORK_ENTRY')) {
-    define('VINANETWORK_ENTRY', true);
-}
-
 $root_path = __DIR__ . '/../../';
+// constants | logging | config | error | session | database | header-auth
 require_once $root_path . 'acc/bootstrap.php';
 
 // Set response header
