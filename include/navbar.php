9b1a6571d0817ef483b7151b20c07e639e85cc92
Vina Network <n9u93n@gmail.com>
2025-09-06 09:18:57 +0700

    Update navbar.php

--- include/navbar.php
+++ include/navbar.php
@@ -29,8 +29,8 @@
                 <i class="fas fa-user"></i> Accounts <i class="dropdown-icon fas fa-caret-down"></i>
             </a>
             <ul class="dropdown-menu">
-                <li><a href="/acc" class="dropdown-link"><i class="fas fa-wallet"></i> Connect Wallet</a></li>
-                <li><a href="/acc/profile.php" class="dropdown-link"><i class="fas fa-address-card"></i> Profile</a></li>
+                <li><a href="/acc/connect" class="dropdown-link"><i class="fas fa-wallet"></i> Connect</a></li>
+                <li><a href="/acc/profile" class="dropdown-link"><i class="fas fa-address-card"></i> Profile</a></li>
             </ul>
         </li>
 
