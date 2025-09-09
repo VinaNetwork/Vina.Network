5d2695df9b5a19294c3a43367af798a9937e8c47
Vina Network <n9u93n@gmail.com>
2025-09-09 17:02:44 +0700

    Create bootstrap.php

---
+++ manage/bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+// ============================================================================
+// File: manage/bootstrap.php
+// Description: Security check and utility functions
+// Created by: Vina Network
+// ============================================================================
+
+// Access Conditions
+if (!defined('VINANETWORK_ENTRY')) {
+    define('VINANETWORK_ENTRY', true);
+}
+
+$root_path = __DIR__ . '/../';
+// General configuration
+require_once $root_path . 'core/constants.php'; 		    // Dynamic Domain Name Definition
+require_once $root_path . 'core/logging.php'; 	        // Logging utilities
+require_once $root_path . 'core/config.php'; 			      // Central configuration
+require_once $root_path . 'core/error.php'; 			      // PHP configuration
+require_once $root_path . 'core/session.php'; 		      // Initialize session with security options
+require_once $root_path . 'core/db.php'; 				        // Database connection management
+require_once $root_path . 'core/header-auth.php'; 		  // Security Headers
+?>
