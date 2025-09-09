062c70e987e6a3acab794242bc9a71c927a6a06f
Vina Network <n9u93n@gmail.com>
2025-09-09 17:19:30 +0700

    Update bootstrap.php

--- manage/bootstrap.php
+++ manage/bootstrap.php
@@ -12,11 +12,11 @@ if (!defined('VINANETWORK_ENTRY')) {
 
 $root_path = __DIR__ . '/../';
 // General configuration
-require_once $root_path . 'core/constants.php'; 		    // Dynamic Domain Name Definition
+require_once $root_path . 'core/constants.php'; 		// Dynamic Domain Name Definition
 require_once $root_path . 'core/logging.php'; 	        // Logging utilities
-require_once $root_path . 'core/config.php'; 			      // Central configuration
-require_once $root_path . 'core/error.php'; 			      // PHP configuration
-require_once $root_path . 'core/session.php'; 		      // Initialize session with security options
-require_once $root_path . 'core/db.php'; 				        // Database connection management
-require_once $root_path . 'core/header-auth.php'; 		  // Security Headers
+require_once $root_path . 'core/config.php'; 			// Central configuration
+require_once $root_path . 'core/error.php'; 			// PHP configuration
+require_once $root_path . 'core/session.php'; 		    // Initialize session with security options
+require_once $root_path . 'core/db.php'; 				// Database connection management
+require_once $root_path . 'core/header-auth.php'; 		// Security Headers
 ?>
