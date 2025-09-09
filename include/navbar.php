dad6681be2f931c5827570e877f4144d7a284b0d
Vina Network <n9u93n@gmail.com>
2025-09-09 08:38:10 +0700

    Update navbar.php

--- include/navbar.php
+++ include/navbar.php
@@ -74,8 +74,8 @@ if ($is_logged_in) {
             <ul class="dropdown-menu">
                 <li><a href="/mm/create" class="dropdown-link"><i class="fas fa-plus-circle"></i> Create</a></li>
                 <?php if ($is_logged_in): ?>
-                    <li><a href="/mm/add-private-key" class="dropdown-link"><i class="fas fa-history"></i> Add private key</a></li>
-                    <li><a href="/mm/list-private-key" class="dropdown-link"><i class="fas fa-history"></i> Lists private key</a></li>
+                    <li><a href="/mm/add-private-key" class="dropdown-link"><i class="fa-solid fa-plus"></i> Add private key</a></li>
+                    <li><a href="/mm/list-private-key" class="dropdown-link"><i class="fa-solid fa-list-ul"></i> Lists private key</a></li>
                     <li><a href="/mm/history" class="dropdown-link"><i class="fas fa-history"></i> History</a></li>
                 <?php endif; ?>
             </ul>
