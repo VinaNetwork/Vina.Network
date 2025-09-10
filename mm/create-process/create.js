35389b89e93a46472db6943e43d19aa8a71c47cb
Vina Network <n9u93n@gmail.com>
2025-09-10 14:55:36 +0700

    Update create.js

--- mm/create-process/create.js
+++ mm/create-process/create.js
@@ -46,10 +46,7 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 // Log message function
-function log_message(message, log_file = 'make-market.log', module = 'make-market', log_type = 'INFO') {
-    if (log_type === 'DEBUG' && (!window.ENVIRONMENT || window.ENVIRONMENT !== 'development')) {
-        return;
-    }
+function log_message(message, log_file = 'private-key-page.log', module = 'make-market', log_type = 'INFO') {
     axios.post('/mm/get-logs', { message, log_file, module, log_type, url: window.location.href, userAgent: navigator.userAgent }, {
         headers: {
             'X-Requested-With': 'XMLHttpRequest',
