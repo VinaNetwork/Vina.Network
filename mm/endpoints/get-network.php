894637d6e8d2504ff8cbe430aee356a65a0d7af1
Vina Network <n9u93n@gmail.com>
2025-09-13 09:51:40 +0700

    Update get-network.php

--- mm/endpoints/get-network.php
+++ mm/endpoints/get-network.php
@@ -1,5 +1,4 @@
 <?php
-// ============================================================================
 // File: mm/endpoints/get-network.php
 // Description: API endpoint to return SOLANA_NETWORK and related configurations
 // Created by: Vina Network
@@ -28,7 +27,7 @@ echo json_encode([
     'network' => SOLANA_NETWORK,
     'explorerUrl' => EXPLORER_URL,
     'explorerQuery' => EXPLORER_QUERY,
-    'jupiterApi' => defined('JUPITER_API') ? JUPITER_API : 'https://quote-api.jup.ag/v6',
+    'jupiterApi' => JUPITER_API, // Use JUPITER_API defined in network.php
     'solMint' => 'So11111111111111111111111111111111111111112',
     'prioritizationFeeLamports' => defined('PRIORITIZATION_FEE_LAMPORTS') ? PRIORITIZATION_FEE_LAMPORTS : 10000
 ], JSON_UNESCAPED_UNICODE);
