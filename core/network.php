944aab3c697745f0034a7261ada36b94addc9289
Vina Network <n9u93n@gmail.com>
2025-09-13 17:09:04 +0700

    Add files via upload

--- core/network.php
+++ core/network.php
@@ -54,9 +54,12 @@ if (!defined('EXPLORER_QUERY')) {
     });
 }
 
-// Define Jupiter API (1 endpoint)
+// Define Jupiter API
 if (!defined('JUPITER_API')) {
-    define('JUPITER_API', 'https://quote-api.jup.ag/v6/quote');
+    define('JUPITER_API', match (SOLANA_NETWORK) {
+        'devnet'  => 'https://quote-api.jup.ag/v6/quote?network=devnet',
+        'mainnet' => 'https://quote-api.jup.ag/v6/quote',
+    });
 }
 
 // Log loaded configuration
